04730f392e4d6e9fb72807146c5a19cce346001a
Philip Windridge <p.c.windridge@staffs.ac.uk>
2018-01-30 15:33:33 +0000

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Welcome Page</title>
+</head>
+<body>
+<header>
+    <h1>Welcome to PHP</h1>
+</header>
+</body>
+</html>
